96a05bcc58a7ebc82a50e0c0e683e1f1b8213ea0
MarthaLRigby <martha.l.rigby@gmail.com>
2015-06-03 00:39:40 +0100

    Ex 5-2

---
+++ 5-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input> ");
+            int myInt = Convert.ToInt32(Console.ReadLine());
+            if (myInt > 100)
+            {
+                Console.WriteLine("Too Large");
+            }
+            else
+            {
+                if (myInt % 2 == 0)
+                {
+                    
+                    if (myInt % 10 == 0)
+                    {
+                        
+                        if (myInt == 0)
+                        {
+                            Console.WriteLine("Equal to 0");
+                        }
+                        else Console.WriteLine("Multiple of 10");
+                    }
+                    else Console.WriteLine("even");
+                }
+                else Console.WriteLine("odd");
+            }
+        }
+    }
+}
